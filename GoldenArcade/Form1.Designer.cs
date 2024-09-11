365ee784feeea8a300ec772d2dd52501510f0137
Om-Patel31 <om31digital@gmail.com>
2024-09-11 11:18:51 -0400

    Add project files.

---
+++ GoldenArcade/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GoldenArcade
+{
+    partial class GoldenArcade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoldenArcade));
+            this.playButton = new System.Windows.Forms.Button();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.goLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(100, 68);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(161, 73);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.White;
+            this.timerLabel.Location = new System.Drawing.Point(65, 160);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(221, 29);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "Game will start in: 3";
+            // 
+            // goLabel
+            // 
+            this.goLabel.AutoSize = true;
+            this.goLabel.BackColor = System.Drawing.Color.Transparent;
+            this.goLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goLabel.ForeColor = System.Drawing.Color.White;
+            this.goLabel.Location = new System.Drawing.Point(128, 200);
+            this.goLabel.Name = "goLabel";
+            this.goLabel.Size = new System.Drawing.Size(96, 38);
+            this.goLabel.TabIndex = 3;
+            this.goLabel.Text = "GO!!!";
+            this.goLabel.Click += new System.EventHandler(this.goLabel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(267, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GoldenArcade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(367, 288);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.goLabel);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GoldenArcade";
+            this.Text = "GoldenArcade";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Label goLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GoldenArcade/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace GoldenArcade
+{
+    public partial class GoldenArcade : Form
+    {
+        public GoldenArcade()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerLabel.Hide();
+            goLabel.Hide();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playButton.Hide();
+            timerLabel.Show();
+
+            Refresh();
+            Thread.Sleep(1000);
+            timerLabel.Text = "Game will start in: 2";
+
+            Refresh();
+            Thread.Sleep(1000);
+            timerLabel.Text = "Game will start in: 1";
+
+            Refresh();
+            Thread.Sleep(1000);
+            timerLabel.Hide();
+
+            this.BackColor = Color.Green;
+            goLabel.Show();
+        }
+
+        private void goLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
